547f14e437033717b327bcb857d00d1b1f6751e3
ijeoma-dev <i-enem@leadway.com>
2022-10-31 09:56:01 +0100

    Added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './core/rooms/rooms.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoomsComponent
+    RoomsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
